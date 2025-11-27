370836ed81c81aa6e6c6c1fd914210a34405afaf
KaioDev <kaiomarcio.dev@gmail.com>
2025-11-27 08:04:02 -0300

    Create tenant module of reposytori

---
+++ src/modules/tenant/repository.js
@@ -0,0 +1,20 @@
+import {pool} from ',,/../config/db.js'
+
+export class TenantRepository{
+    async create({name}){
+        const {rows} = await pool.query(
+            'INSERT INTO tenants (nome) VALUES ($1), RETURNING*',
+            [name]
+        )
+        return rows[0]
+    }
+
+    async findByName(name){
+        const {rows} = await pool.query(
+            'SELECT * FROM tenants WHERE nome = $1',
+            [name]
+        )
+        return rows[0]
+    }
+
+}
