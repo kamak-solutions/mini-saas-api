b2089cf317528db248066108d9eeba1f5c26af97
KaioDev <kaiomarcio.dev@gmail.com>
2025-12-08 00:20:14 -0300

    deploy: adiciona CORS dentro de middlewares()

--- src/app.js
+++ src/app.js
@@ -1,40 +1,36 @@
-import express from 'express'
-import helmet from 'helmet'
-import routes from './routes/index.js'
-import {logger} from './config/logger.js'
-import cookieParser from 'cookie-parser';
+import express from "express";
+import helmet from "helmet";
+import routes from "./routes/index.js";
+import { logger } from "./config/logger.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-export class App{
-    constructor(){
-        this.express = express()
-        this.middlewares()
-        this.routes()
-        
-    }
-    middlewares(){
-        this.express.use(express.json())
+export class App {
+  constructor() {
+    this.express = express();
+    this.middlewares();
+    this.routes();
+  }
+  middlewares() {
+    this.express.use(express.json());
 
-         // Segurança com Helmet
-        this.express.use(helmet())
-        this.express.use(cookieParser())
-       
-    
-    }
-    routes() {
-  console.log('1 - entrando em routes');
-  this.express.use('/api', routes);
-  console.log('2 - use /api foi chamado');
-}
+    // Segurança com Helmet
+    this.express.use(helmet());
+    this.express.use(cookieParser());
+    this.express.use(cors({
+    origin: ['http://localhost:3000', 'https://seu-front.netlify.app'],
+    credentials: true,
+  }));
+  }
+  routes() {
+    console.log("1 - entrando em routes");
+    this.express.use("/api", routes);
+    console.log("2 - use /api foi chamado");
+  }
 
-    
-
-   
-    listen(port){
-        return this.express.listen(port, ()=>{
-           logger.info(`API em http://localhost:${port}`);
-            
-        })
-
-        
-    }
+  listen(port) {
+    return this.express.listen(port, () => {
+      logger.info(`API em http://localhost:${port}`);
+    });
+  }
 }
